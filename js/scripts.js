f5619ffaa08f5803f822c68950cdec443852f638
ganju-simran <ganjusimran97@gmail.com>
2021-08-14 17:33:09 +0530

    moved fetch to one common function

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ let pageIndexState = 1;
 let isLoading = false;
 const searchSubmit = document.querySelector(".search-input");
 const serchText = document.querySelector("#search-text");
+let isSearching = false;
 
 // function fetch the movie data
-const fetchPageData = (pageIndex = pageIndexState) => {
+const fetchPageData = (queryText) => {
     const movieCards = document.querySelector(".card-container");
     let htmlCode = `
         <div class="loader">
@@ -13,7 +14,7 @@ const fetchPageData = (pageIndex = pageIndexState) => {
     `
     isLoading = true;
     movieCards.innerHTML = movieCards.innerHTML + htmlCode;
-    return fetch(`https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=&cl=all&p=${pageIndex}&t=ns&st=adv `, {
+    return fetch(queryText ? `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=${queryText}&cl=all&p=${pageIndexState}&t=ns&st=adv ` : `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=&cl=all&p=${pageIndexState}&t=ns&st=adv `, {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "3afe754955mshc08cfb8a4e878b2p1f5b3cjsna3f5c2cc9959",
@@ -22,8 +23,11 @@ const fetchPageData = (pageIndex = pageIndexState) => {
     })
         .then(response =>
             response.json().then(result => {
+                if (queryText && result.ITEMS.length == result.COUNT) {
+                    isSearching = false;
+                }
                 pageIndexState += 1;
-                renderMovieCards(result, !(pageIndex > 1));
+                renderMovieCards(result, !(pageIndexState > 1));
             })
         )
         .catch(err => {
@@ -32,32 +36,14 @@ const fetchPageData = (pageIndex = pageIndexState) => {
 }
 
 // function fetch the movie data once user searches
-const fetchSearchData = (pageIndex = 1) => {
-    const searchText = serchText.value || "get:new7";
+const fetchSearchData = () => {
     const movieCards = document.querySelector(".card-container");
-    let htmlCode = `
-        <div class="loader">
-            <img  alt="loader.gif" src="https://i.gifer.com/origin/dd/dd9538796fae4795531f8219af8a181c.gif">
-        </div>
-    `
-    isLoading = true;
-    movieCards.innerHTML = htmlCode;
-    return fetch(`https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=${searchText}-!1900%2C2018-!0%2C5-!0%2C10-!0-!Any-!Any-!Any-!gt100-!%7Bdownloadable%7D&t=ns&cl=all&st=adv&ob=Relevance&p=${pageIndex}&sa=and`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "3afe754955mshc08cfb8a4e878b2p1f5b3cjsna3f5c2cc9959",
-            "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com"
-        }
-    })
-        .then(response =>
-            response.json().then(result => {
-                pageIndexState += 1;
-                renderMovieCards(result, !(pageIndex > 1));
-            })
-        )
-        .catch(err => {
-            console.error(err);
-        });
+    movieCards.innerHTML = '';
+    pageIndexState = 1;
+    const searchValue = serchText.value;
+    if (!searchValue) isSearching = false;
+    else isSearching = true;
+    fetchPageData(searchValue);
 }
 
 // function to render cards once movie data is fetched
@@ -85,13 +71,11 @@ const renderMovieCards = (movieData, initial = true) => {
 document.querySelector(".card-container").addEventListener("scroll", function () {
     var myDiv = document.querySelector(".card-container");
     if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight && !isLoading) {
-        fetchPageData();
+        fetchPageData(isSearching ? serchText.value : null);
     }
 });
 
 window.onload = () => {
-    console.log("hgjhjgjkjkjhjkkjbkjjkkjbkjbjkjhg");
-
     // fetch page data is called when page is rendered
     fetchPageData();
 };
